4a0218d5d167dfb92f9ebbc2c07486c986b80673
AleSbrissia <alexandresbrissia@ufpr.br>
2026-09-20 19:17:34 -0300

    colocando restricoes

--- Trabalho1/main.cpp
+++ Trabalho1/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std ;
 
 
+struct Cabo{
+    // indice da central, capacidade, custo, vazao de energia
+    long long t, w, c, v;
+};
 struct HidroEletrica {
     // capacidade, eficiencia, custo, vazao de agua, numero de cabos
     long long m, f, c, v, n;
@@ -12,10 +17,6 @@ struct Central{
     long long d, n; 
     Cabo *a;
 };
-struct Cabo{
-    // indice da central, capacidade, custo, vazao de energia
-    long long t, w, c, v;
-};
 
 int main () {
 
@@ -23,6 +24,8 @@ int main () {
     int i, j ;
     HidroEletrica *Hidros;
     Central *Centrais;
+
+    cin >> h >> l >> r ;
 
     Hidros = (HidroEletrica *)malloc( sizeof(HidroEletrica) * h);
     Centrais = (Central *)malloc( sizeof(Central) * l);
@@ -57,27 +60,105 @@ int main () {
     cout << "min : " ;
     /*custo das hidreletricas*/
     for(i = 0; i < h; i++) {
-        cout << Hidros[i].c << "x" << i +1 << " + ";
-    }
+        if (i > 0) cout << " + ";
+        cout << Hidros[i].c << "x" << i +1;
+    } 
 
-    /*custo de cada cabo das hidreletricas*/
-    for(i = 0; i < h; i++) {
-        for(j = 0; j < Hidros[i].n; j++) {
-            cout << Hidros[i].a[j].c << "y" << i +1 << " + ";
+    long long k = 1;
+
+    /* custo dos cabos das hidrelétricas */
+    for (i = 0; i < h; i++) {
+        for (j = 0; j < Hidros[i].n; j++) {
+            cout << " + " << Hidros[i].a[j].c << " y" << k++;
         }
     }
 
-    /*custo de cada cabo das centrais*/
-    for(i = 0; i < l; i++) {
-        for(j = 0; j < Centrais[i].n -1; j++) {
-            cout << Centrais[i].a[j].c << "y" << i +1 +h << " + ";
+    /* custo dos cabos das centrais */
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < Centrais[i].n; j++) {
+            cout << " + " << Centrais[i].a[j].c << " y" << k++;
         }
-        cout << Centrais[i].a[j].c << "y" << i +1 +h ;
     }
-    cout << endl ;
 
-    
+    cout << ";\n";
+        
+    /* Vazao e capacidade de producao */
+    for (i = 0; i < h; i++) {
+        cout << "x" << i + 1 << " >= 0;\n";
+        cout << "x" << i + 1 << " <= " << r << ";\n";
 
+        cout << Hidros[i].f << " x" << i + 1
+            << " <= " << Hidros[i].m << ";\n";
+    }
+
+    /* Energia produzida = soma da energia enviada */
+    k = 1;
+
+    for (i = 0; i < h; i++) {
+        cout << Hidros[i].f << " x" << i + 1;
+
+        for (j = 0; j < Hidros[i].n; j++) {
+            cout << " - y" << k++;
+        }
+
+        cout << " = 0;\n";
+    }
+
+    /* Capacidades dos cabos, na ordem da funcao objetivo */
+    k = 1;
+
+    for (i = 0; i < h; i++) {
+        for (j = 0; j < Hidros[i].n; j++) {
+            cout << "y" << k << " >= 0;\n";
+            cout << "y" << k << " <= "
+                << Hidros[i].a[j].w << ";\n";
+            k++;
+        }
+    }
+
+    for (i = 0; i < l; i++) {
+        for (j = 0; j < Centrais[i].n; j++) {
+            cout << "y" << k << " >= 0;\n";
+            cout << "y" << k << " <= "
+                << Centrais[i].a[j].w << ";\n";
+            k++;
+        }
+    }
+
+    /* Atendimento das demandas: entradas - saidas >= demanda */
+    for (int central = 0; central < l; central++) {
+        k = 1;
+
+        // Comecar com zero simplifica a impressao dos sinais.
+        cout << "0";
+
+        /* Entradas vindas das hidreletricas */
+        for (i = 0; i < h; i++) {
+            for (j = 0; j < Hidros[i].n; j++) {
+                if (Hidros[i].a[j].t == central + 1) {
+                    cout << " + y" << k;
+                }
+                k++;
+            }
+        }
+
+        /* Entradas e saidas envolvendo centrais */
+        for (i = 0; i < l; i++) {
+            for (j = 0; j < Centrais[i].n; j++) {
+                if (Centrais[i].a[j].t == central + 1) {
+                    cout << " + y" << k;
+                }
+
+                if (i == central) {
+                    cout << " - y" << k;
+                }
+
+                k++;
+            }
+        }
+
+        cout << " >= " << Centrais[central].d << ";\n";
+    }
 
 
 
